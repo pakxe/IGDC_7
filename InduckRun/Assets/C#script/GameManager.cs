387810ff65b182b598b847a1975025383bdd37a5
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2021-07-26 13:37:29 +0900

    인덕런연결본 이걸로 수정해주세요!!

---
+++ InduckRun/Assets/C#script/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int[] foodGoldList;
+    public Sprite[] TrashSprite;
+
+    public Sprite[] Background;
+
+    public Sprite[] Preview;
+    public int[] InduckID;
+    public int[] InduckGold;
+    public string[] InduckName;
+    public Sprite[] InduckSprite;
+
+    public Vector3[] PointList;
+    public Vector3[] TrashPoint;
+}
